6a37cf4a8899f278c55dc1ededb9b592a2cee42d
Marc Peace <marc.peace@symbio.com>
2017-05-02 14:26:18 +0200

    bitnami DB in config files

--- config/config.php
+++ config/config.php
@@ -106,9 +106,9 @@
   * Database configuration
   */
  $conf['settings']['database']['type'] = 'mysql';
- $conf['settings']['database']['user'] = 'be60bd98781dca';        // database user with permission to the booked database
- $conf['settings']['database']['password'] = '0cfdab32f62f323';
- $conf['settings']['database']['hostspec'] = 'eu-cdbr-azure-west-d.cloudapp.net';        // ip, dns or named pipe
+ $conf['settings']['database']['user'] = 'root';        // database user with permission to the booked database
+ $conf['settings']['database']['password'] = 'xJylY10OJXbb';
+ $conf['settings']['database']['hostspec'] = '127.0.0.1';        // ip, dns or named pipe
  $conf['settings']['database']['name'] = 'slowbro';
  /**
   * Mail server configuration
