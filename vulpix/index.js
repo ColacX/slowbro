68d7d1813409a7ec79453c3b00fc162715d69800
Daniel Zheng <danielzheng1989@gmail.com>
2017-05-23 14:02:02 +0200

    improved error handling fixed problem catch and fail always executed the timeout

--- vulpix/index.js
+++ vulpix/index.js
@@ -465,19 +465,23 @@ function repeatInit(){
 	.then(fetchToken)
 	.then(fetchResourceData)
 	.then(fetchReservationsData)
-	.then(function(){
-		startIntervals();
-		setHandlers();
-		hideLoading();
-	})
-	.catch(setTimeout(repeatInit, 10 * 1000));
+	.then(startIntervals)
+	.then(setHandlers)
+	.then(hideLoading)
+	.catch(function(e){
+		console.error(e);
+		setTimeout(repeatInit, 10 * 1000)
+	});
 }
 
 function repeatFetchReservationData(){
 	console.log("repeatFetchReservationData");
 	return fetchToken()
 	.then(fetchReservationsData)
-	.fail(setTimeout(repeatFetchReservationData, 10 * 1000));
+	.fail(function(e){
+		console.error(e);
+		setTimeout(repeatFetchReservationData, 10 * 1000);
+	});
 }
 
 $(document).ready(function(){
