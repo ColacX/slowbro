59a73269c2a2bda3d9039addd28081cf736df80e
Daniel Zheng <danielzheng1989@gmail.com>
2017-05-23 14:03:28 +0200

    update version date

--- vulpix/index.js
+++ vulpix/index.js
@@ -1,4 +1,4 @@
-console.log("version datetime: Tue May 23 2017 13:49:16 GMT+0200 (W. Europe Daylight Time)");
+console.log("version datetime: Tue May 23 2017 14:02:56 GMT+0200 (W. Europe Daylight Time)");
 	
 var locale = "SV-sv";
 var sessionToken = null;
@@ -501,5 +501,5 @@ $(document).ready(function(){
 	}
 	
 	showLoading();
-	//repeatInit();
+	repeatInit();
 });
