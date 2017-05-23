c3a94170b24177d9bd74857d3a3402c2569ce827
Daniel Zheng <danielzheng1989@gmail.com>
2017-05-23 14:16:32 +0200

    removed push notifications since it was not supported by webview

--- vulpix/index.js
+++ vulpix/index.js
@@ -6,8 +6,6 @@ var sessionUserId = null;
 var resourceId = null;
 var username = "tablet";
 var password = "tab1234!";
-var pushServicePublicKey = "BOgNlIommuYhTrYgld5WV6r3k2Z1kO0rzbfNgLoWDhDb-prQNln-aWUdHSAazgMlghTt-1ZB-ejKz4u57Pi1V9s";
-var swRegistration = null;
 var stateStack = [];
 
 function updateTime(){
@@ -340,11 +338,6 @@ function hideLoading(){
 	$("#loadWindow").hide();
 }
 
-function vibrate(milliseconds){
-	if(!Android) return;
-	Android.vibrate(milliseconds);
-}
-
 function runHarlemShake(){
 	stateStack.push("harlem");
 	
@@ -356,8 +349,6 @@ function runHarlemShake(){
 	}, 35*1000);
 	
 	javascript:(function(){function c(){var e=document.createElement("link");e.setAttribute("type","text/css");e.setAttribute("rel","stylesheet");e.setAttribute("href",f);e.setAttribute("class",l);document.body.appendChild(e)}function h(){var e=document.getElementsByClassName(l);for(var t=0;t<e.length;t++){document.body.removeChild(e[t])}}function p(){var e=document.createElement("div");e.setAttribute("class",a);document.body.appendChild(e);setTimeout(function(){document.body.removeChild(e)},100)}function d(e){return{height:e.offsetHeight,width:e.offsetWidth}}function v(i){var s=d(i);return s.height>e&&s.height<n&&s.width>t&&s.width<r}function m(e){var t=e;var n=0;while(!!t){n+=t.offsetTop;t=t.offsetParent}return n}function g(){var e=document.documentElement;if(!!window.innerWidth){return window.innerHeight}else if(e&&!isNaN(e.clientHeight)){return e.clientHeight}return 0}function y(){if(window.pageYOffset){return window.pageYOffset}return Math.max(document.documentElement.scrollTop,document.body.scrollTop)}function E(e){var t=m(e);return t>=w&&t<=b+w}function S(){var e=document.createElement("audio");e.setAttribute("class",l);e.src=i;e.loop=false;e.addEventListener("canplay",function(){setTimeout(function(){x(k)},500);setTimeout(function(){N();p();for(var e=0;e<O.length;e++){T(O[e])}},15500)},true);e.addEventListener("ended",function(){N();h()},true);e.innerHTML=" <p>If you are reading this, it is because your browser does not support the audio element. We recommend that you get a new browser.</p> <p>";document.body.appendChild(e);e.play()}function x(e){e.className+=" "+s+" "+o}function T(e){e.className+=" "+s+" "+u[Math.floor(Math.random()*u.length)]}function N(){var e=document.getElementsByClassName(s);var t=new RegExp("\\b"+s+"\\b");for(var n=0;n<e.length;){e[n].className=e[n].className.replace(t,"")}}var e=0;var t=0;var n=500;var r=500;var i="//s3.amazonaws.com/moovweb-marketing/playground/harlem-shake.mp3";var s="mw-harlem_shake_me";var o="im_first";var u=["im_drunk","im_baked","im_trippin","im_blown"];var a="mw-strobe_light";var f="//s3.amazonaws.com/moovweb-marketing/playground/harlem-shake-style.css";var l="mw_added_css";var b=g();var w=y();var C=document.getElementsByTagName("*");var k=null;for(var L=0;L<C.length;L++){var A=C[L];if(v(A)){if(E(A)){k=A;break}}}if(A===null){console.warn("Could not find a node of the right size. Please try a different page.");return}c();S();var O=[];for(var L=0;L<C.length;L++){var A=C[L];if(v(A)){O.push(A)}}})();
-
-	vibrate(5000);
 }
 
 function checkVideo(){
@@ -381,46 +372,6 @@ function urlB64ToUint8Array(base64String) {
 	}
 	
 	return outputArray;
-}
-
-function registerServiceWorker(){
-	if(!("serviceWorker" in navigator)) {
-		alert("Service worker is not supported");
-	}
-	
-	if(!("PushManager" in window)){
-		alert("Push is not supported");
-	}
-	
-	return navigator.serviceWorker.register("serviceWorker.js")
-	.then(function(swr) {
-		swRegistration = swr;
-		console.log("Service Worker is registered", swr);
-	})
-	.catch(function(e) {
-		console.error("Service Worker Error", e);
-	});
-}
-
-function unregisterServiceWorker(){
-}
-
-function registerPushService(){
-	return swRegistration.pushManager.subscribe({
-		userVisibleOnly: true,
-		applicationServerKey: urlB64ToUint8Array(pushServicePublicKey)
-	})
-	.then(function(subscription) {
-		console.log("Push subscription succeeded");
-		console.log(subscription);
-		console.dir(JSON.stringify(subscription));
-	})
-	.catch(function(e) {
-		console.error('Failed to subscribe to the push service', e);
-	});
-}
-
-function unregisterPushService(){
 }
 
 function startIntervals()
@@ -459,10 +410,7 @@ function setHandlers(){
 }
 
 function repeatInit(){
-	console.log("repeatInit");
-	return registerServiceWorker()
-	.then(registerPushService)
-	.then(fetchToken)
+	return fetchToken()
 	.then(fetchResourceData)
 	.then(fetchReservationsData)
 	.then(startIntervals)
@@ -475,7 +423,6 @@ function repeatInit(){
 }
 
 function repeatFetchReservationData(){
-	console.log("repeatFetchReservationData");
 	return fetchToken()
 	.then(fetchReservationsData)
 	.fail(function(e){
@@ -499,7 +446,8 @@ $(document).ready(function(){
 		//mock data below
 		updateReservationView(mockdata.reservations);
 	}
-	
-	showLoading();
-	repeatInit();
+	else{
+		showLoading();
+		repeatInit();
+	}
 });
